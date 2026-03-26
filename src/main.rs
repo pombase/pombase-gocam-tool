9ca2c5b5c4dd1f12275b63505aefc7927b8e7e92
Kim Rutherford <kmr@aska.gen.nz>
2026-03-26 19:13:27 +1300

    Report err. message before panicking while parsing
    We were just calling .unwrap()
    
    Refs pombase/pombase-gocam#28

--- src/main.rs
+++ src/main.rs
@@ -329,7 +329,13 @@ fn model_from_path(path: &PathBuf) -> GoCamModel {
     if path.extension().unwrap() == "json" {
         parse_raw_gocam_model(&mut source).unwrap()
     } else {
-        parse_gocam_py_model(&mut source).unwrap()
+        eprintln!("path: {}", path.to_str().unwrap());
+        let res = parse_gocam_py_model(&mut source);
+
+        match res {
+            Ok(model) => model,
+            Err(err) => panic!("{}", err),
+        }
     }
 }
 

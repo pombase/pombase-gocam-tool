a5f66c4985c62707e9be469e7634c2f7f5ba1ed8
Kim Rutherford <kmr@aska.gen.nz>
2026-04-01 17:42:08 +1300

    Write GAF BP and CC annotation from GO-CAMs
    Refs pombase/pombase-chado#1340

--- src/go_format_writer.rs
+++ src/go_format_writer.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, io::{BufWriter, Error, Write}};
 
 use chrono::{DateTime, Local};
 use itertools::Itertools;
-use pombase_gocam::gocam_py::GoCamPyModel;
+use pombase_gocam::gocam_py::{EvidenceItem, GoCamPyModel};
 
 fn make_annotation_line(db_name: &str, gene_uniquename: &str,
                         term_id: &str, aspect: &str,
@@ -36,27 +36,76 @@ pub fn write_go_annotation_file(writer: &mut dyn Write,
         local.format("%F").to_string()
     };
 
+    let details_from_item = |evidence_item: &EvidenceItem| {
+        let Some(reference) = &evidence_item.reference
+        else {
+            return None;
+        };
+
+        let ev_code = &evidence_item.term;
+
+        if !reference.starts_with("GO_REF:") {
+            return None;
+        }
+
+        let Some(go_ev_code) = go_evidence_code_map.get(ev_code)
+        else {
+            panic!("unknown evidence code: {}", ev_code);
+        };
+
+        let with_from = evidence_item.with_objects.iter().join(",");
+
+        Some((go_ev_code, reference.to_owned(), with_from))
+    };
+
+
     for activity in &model.activities {
         let gene_uniquename = &activity.enabled_by.term;
         let mf_term_id = &activity.molecular_function.term;
 
-        if let Some(mf_evidence_item) = activity.enabled_by.evidence.get(0) &&
-           let Some(reference) = &mf_evidence_item.reference {
-
-            let ev_code = &mf_evidence_item.term;
-            let with_from = mf_evidence_item.with_objects.iter().join(",");
-
-            if !reference.starts_with("GO_REF:") {
-                continue;
-            }
-
-            let Some(go_ev_code) = go_evidence_code_map.get(ev_code)
+        if let Some(evidence_item) = activity.enabled_by.evidence.get(0) {
+            let Some((go_ev_code, reference, with_from)) = details_from_item(evidence_item)
             else {
-                panic!("unknown evidence code: {}", ev_code);
+                continue;
             };
 
             let line = make_annotation_line(db_name, gene_uniquename, mf_term_id,
-                                            "F", reference,
+                                            "F", &reference,
+                                            go_ev_code, &with_from, &taxon_id, &date_modified);
+
+            writeln!(buf_writer, "{}", line)?;
+        }
+
+        if let Some(ref bp_association) = activity.part_of &&
+           let Some(evidence_item) = bp_association.evidence.get(0) {
+
+            let bp_term_id = &bp_association.term;
+
+            let Some((go_ev_code, reference, with_from)) = details_from_item(evidence_item)
+            else {
+                continue;
+            };
+
+            let line = make_annotation_line(db_name, gene_uniquename, bp_term_id,
+                                            "P", &reference,
+                                            go_ev_code, &with_from, &taxon_id, &date_modified);
+
+            writeln!(buf_writer, "{}", line)?;
+
+        }
+
+        if let Some(ref cc_assocation) = activity.occurs_in &&
+            let Some(evidence_item) = cc_assocation.evidence.get(0) {
+
+            let cc_term_id = &cc_assocation.term;
+
+            let Some((go_ev_code, reference, with_from)) = details_from_item(evidence_item)
+            else {
+                continue;
+            };
+
+            let line = make_annotation_line(db_name, gene_uniquename, cc_term_id,
+                                            "C", &reference,
                                             go_ev_code, &with_from, &taxon_id, &date_modified);
 
             writeln!(buf_writer, "{}", line)?;

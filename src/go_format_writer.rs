237e67f7b0117fc33b09faa3e3043bf6e4ed5db9
Kim Rutherford <kmr@aska.gen.nz>
2026-06-11 20:56:06 +1200

    Better error reporting for unknown evidence codes
    Refs pombase/website#2506

--- src/go_format_writer.rs
+++ src/go_format_writer.rs
@@ -19,32 +19,31 @@ fn make_annotation_line(db_name: &str, gene_uniquename: &str,
 
 fn details_from_item(go_evidence_code_map: &HashMap<String, String>,
                      evidence_item: &EvidenceItem)
-    -> Option<(String, String, String)>
+    -> Result<Option<(String, String, String)>, String>
 {
     let Some(reference) = &evidence_item.reference
     else {
-        return None;
+        return Ok(None);
     };
 
     let ev_code = &evidence_item.term;
 
     if ev_code == ND_ECO_TERM_ID {
-        return None;
+        return Ok(None);
     }
 
     let Some(go_ev_code) = go_evidence_code_map.get(ev_code)
     else {
-        eprintln!("unknown evidence code: {}", ev_code);
-        return None;
+        return Err(format!("unknown evidence code: {}", ev_code));
     };
 
     if !reference.starts_with("GO_REF:") && ev_code != "ECO:0000304" {
-        return None;
+        return Ok(None);
     }
 
     let with_from = evidence_item.with_objects.iter().join(",");
 
-    Some((go_ev_code.to_owned(), reference.to_owned(), with_from))
+    Ok(Some((go_ev_code.to_owned(), reference.to_owned(), with_from)))
 }
 
 
@@ -52,7 +51,7 @@ fn bp_association_lines(go_ev_code_map: &HashMap<String, String>,
                         db_name: &str, taxon_id: &str,
                         gene_uniquename: &str, date_modified: &str,
                         bp_association: &BiologicalProcessAssociation)
-    -> Vec<String>
+    -> Result<Vec<String>, String>
 {
     let mut ret = vec![];
 
@@ -61,7 +60,9 @@ fn bp_association_lines(go_ev_code_map: &HashMap<String, String>,
 
         let bp_term_id = &bp_association.term;
 
-        if let Some((go_ev_code, reference, with_from)) = details_from_item(go_ev_code_map, evidence_item) {
+        let details = details_from_item(go_ev_code_map, evidence_item)?;
+
+        if let Some((go_ev_code, reference, with_from)) = details {
             let line = make_annotation_line(db_name, gene_uniquename, bp_term_id,
                                             "P", &reference,
                                             &go_ev_code, &with_from, taxon_id, date_modified);
@@ -72,12 +73,12 @@ fn bp_association_lines(go_ev_code_map: &HashMap<String, String>,
         if let Some(ref bp_association) = bp_association.part_of {
             let lines =
                 bp_association_lines(go_ev_code_map, db_name, taxon_id,
-                                     gene_uniquename, date_modified, bp_association);
+                                     gene_uniquename, date_modified, bp_association)?;
             ret.extend_from_slice(&lines);
         }
     }
 
-    ret
+    Ok(ret)
 }
 
 pub fn write_go_annotation_file(writer: &mut dyn Write,
@@ -112,24 +113,40 @@ pub fn write_go_annotation_file(writer: &mut dyn Write,
 
         let mf_term_id = &activity.molecular_function.term;
 
-        if let Some(evidence_item) = activity.enabled_by.evidence.first() &&
-            let Some((go_ev_code, reference, with_from)) = details_from_item(go_ev_code_map, evidence_item)
-        {
-            let line = make_annotation_line(db_name, gene_uniquename, mf_term_id,
-                                            "F", &reference,
-                                            &go_ev_code, &with_from, &taxon_id, &date_modified);
+        if let Some(evidence_item) = activity.enabled_by.evidence.first() {
+            match details_from_item(go_ev_code_map, evidence_item) {
+                Ok(details) => {
+                    if let Some((go_ev_code, reference, with_from)) = details {
+                        let line = make_annotation_line(db_name, gene_uniquename, mf_term_id,
+                                                        "F", &reference,
+                                                        &go_ev_code, &with_from, &taxon_id, &date_modified);
 
-            writeln!(buf_writer, "{}", line)?;
+                        writeln!(buf_writer, "{}", line)?;
+                    }
+                },
+                Err(message) => {
+                    eprintln!("warning in {} for {} {}: {}",
+                              model.id, mf_term_id, gene_uniquename, message);
+                }
+            };
         }
 
         if let Some(ref bp_association) = activity.part_of {
-            let bp_lines =
+            let bp_lines_result =
                 bp_association_lines(go_ev_code_map, db_name, &taxon_id,
                                      gene_uniquename, &date_modified, bp_association);
 
-            for line in bp_lines {
-                writeln!(buf_writer, "{}", line)?;
-            }
+            match bp_lines_result {
+                Ok(bp_lines) => {
+                    for line in bp_lines {
+                        writeln!(buf_writer, "{}", line)?;
+                    }
+                },
+                Err(message) => {
+                    eprintln!("warning in {} for {} {}: {}",
+                              model.id, mf_term_id, gene_uniquename, message);
+                }
+            };
         }
 
         if let Some(ref cc_assocation) = activity.occurs_in &&
@@ -137,7 +154,16 @@ pub fn write_go_annotation_file(writer: &mut dyn Write,
 
             let cc_term_id = &cc_assocation.term;
 
-            let Some((go_ev_code, reference, with_from)) = details_from_item(go_ev_code_map, evidence_item)
+            let details = match details_from_item(go_ev_code_map, evidence_item) {
+                Ok(details) => details,
+                Err(message) => {
+                    eprintln!("warning in {} for {} {}: {}",
+                              model.id, mf_term_id, gene_uniquename, message);
+                    continue;
+                }
+            };
+
+            let Some((go_ev_code, reference, with_from)) = details
             else {
                 continue;
             };

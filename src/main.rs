117030c1306539dc0d574c7001574835ef4bdb51
Kim Rutherford <kmr@aska.gen.nz>
2026-09-09 23:19:01 +1200

    Add total-stats sub-command
    Refs pombase/pombase-gocam#147

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,12 @@ struct Args {
 #[derive(Subcommand)]
 enum Action {
     #[command(arg_required_else_help = true)]
-    Stats {
+    PerModelStats {
+        #[arg(required = true)]
+        paths: Vec<PathBuf>,
+    },
+    #[command(arg_required_else_help = true)]
+    TotalStats {
         #[arg(required = true)]
         paths: Vec<PathBuf>,
     },
@@ -548,7 +553,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match args.action {
-        Action::Stats { paths } => {
+        Action::PerModelStats { paths } => {
             println!("model_id\ttaxon\ttotal_genes\ttotal_complexes\t\
                       connected_genes\tmax_connected_activities\t\
                       total_connected_activities\tnumber_of_holes");
@@ -562,6 +567,93 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                          stats.max_connected_activities,
                          stats.total_connected_activities, stats.number_of_holes);
             }
+        }
+        Action::TotalStats { paths } => {
+            let mut raw_nodes = 0;
+            let mut raw_edges = 0;
+
+            let mut nodes = 0;
+            let mut edges = 0;
+
+            let mut activities = 0;
+            let mut chemicals = 0;
+
+            let mut target_genes = 0;
+
+            let mut total_connected_activities = 0;
+
+            let mut total_go_term_occurrences = 0;
+
+            for path in paths {
+                let mut source = File::open(path).unwrap();
+                let raw_model = gocam_parse_raw(&mut source)?;
+
+                raw_nodes += raw_model.individuals().count();
+                raw_edges += raw_model.facts().count();
+
+                let model = GoCamModel::new_from_raw(raw_model);
+
+                for (_, node) in model.node_iterator() {
+                    nodes += 1;
+
+                    if node.has_process() {
+                        total_go_term_occurrences += 1;
+                    }
+
+                    if node.happens_during.is_some() {
+                        total_go_term_occurrences += 1;
+                    }
+
+                    match node.node_type {
+                        GoCamNodeType::Activity(GoCamActivity { ref enabler, ref inputs, ref outputs }) => {
+                            activities += 1;
+                            total_go_term_occurrences += node.occurs_in.len();
+                            if node.node_id != "GO:0003674" {
+                                total_go_term_occurrences += 1;
+                            }
+                            for input in inputs.iter() {
+                                if input.located_in.is_some() {
+                                    total_go_term_occurrences += 1;
+                                }
+                                if input.is_gene() {
+                                    target_genes += 1;
+                                }
+                            }
+                            for output in outputs.iter() {
+                                if output.located_in.is_some() {
+                                    total_go_term_occurrences += 1;
+                                }
+                                if output.is_gene() {
+                                    target_genes += 1;
+                                }
+                            }
+                        },
+                        GoCamNodeType::Chemical(ref chemical) => {
+                            chemicals += 1;
+                            if chemical.located_in.is_some() {
+                                total_go_term_occurrences += 1;
+                            }
+                        },
+                        _ => (),
+                    }
+                }
+
+                edges += model.edge_iterator().count();
+
+                let model_stats = get_stats(&model);
+
+                total_connected_activities += model_stats.total_connected_activities;
+            }
+
+            println!("raw nodes: {raw_nodes}");
+            println!("raw edges: {raw_edges}");
+            println!("nodes: {nodes}");
+            println!("edges: {edges}");
+            println!("activities: {activities}");
+            println!("connected activities: {total_connected_activities}");
+            println!("target genes: {target_genes}");
+            println!("chemicals: {chemicals}");
+            println!("total GO term occurrences: {total_go_term_occurrences}");
         }
         Action::ConnectedGenes { paths } => {
             println!("taxon\tgene");
@@ -617,7 +709,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let model = gocam_parse_raw(&mut source)?;
                 print_tuples(&model);
             }
-        },
+        }
         Action::PrintIndividuals { paths } => {
             for path in paths {
                 let mut source = File::open(path).unwrap();
